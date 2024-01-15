2b0fb18b13779d00f1fae2eb2a746f44f411e926
Ramandhika <ramandhika.ilham@gmail.com>
2024-01-15 19:03:19 +0700

    Refactor code to improve performance and readability

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Web Portfolio with Tailwind CSS
+
+## Introduction
+
+Hello! This is a my web portfolio built with Tailwind CSS.
+_but it's not finished yet, i will continue this project if i have more free time_
+
+## Images
+
+![Web Portfolio](https://ibb.co/kmZbS6N)
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./public/*.{html,js}'],
+    theme: {
+        extend: {
+            colors: {
+                OxfordBlue: '#0A1128',
+                mikadoYellow: '#FFC800',
+            },
+        },
+    },
+    plugins: [],
+};
